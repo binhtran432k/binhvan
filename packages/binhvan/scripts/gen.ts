d5f20c8aa1d8a683569d1d260fb7cf37d49808ec
Binh Tran <binhtran432k@gmail.com>
2025-01-16 10:52:28 +0700

    refactor(scripts/gen): split plugin into function

--- packages/binhvan/scripts/gen.ts
+++ packages/binhvan/scripts/gen.ts
@@ -1,33 +1,39 @@
+import type { BunPlugin } from "bun";
 import { isolatedDeclaration } from "oxc-transform";
+
+function getTypePlugin(): [plugin: BunPlugin, tsPathsSet: Set<string>] {
+	const tsPathsSet = new Set<string>();
+	const plugin: BunPlugin = {
+		name: "types",
+		setup(builder) {
+			builder.onLoad({ filter: /\.ts$/ }, async (args) => {
+				tsPathsSet.add(args.path);
+			});
+		},
+	};
+	return [plugin, tsPathsSet];
+}
 
 async function main() {
 	await Bun.$`rm -rf dist`;
+	const [typePlugin, tsPathsSet] = getTypePlugin();
 	const result = await Bun.build({
 		entrypoints: ["src/index.ts", "src/components.ts", "src/layouts.ts"],
 		external: ["mini-van-plate"],
-    target: "bun",
+		target: "bun",
 		root: "src",
 		outdir: "dist",
 		splitting: true,
 		minify: true,
-		plugins: [
-			{
-				name: "types",
-				setup(builder) {
-					builder.onLoad({ filter: /\.ts$/ }, async (args) => {
-						const { code } = isolatedDeclaration(
-							args.path,
-							await Bun.file(args.path).text(),
-						);
-						await Bun.write(
-							args.path.replace("src", "dist").replace(/\.ts$/, ".d.ts"),
-							code,
-						);
-					});
-				},
-			},
-		],
+		plugins: [typePlugin],
 	});
+	for (const path of tsPathsSet) {
+		const { code } = isolatedDeclaration(path, await Bun.file(path).text());
+		await Bun.write(
+			path.replace("src", "dist").replace(/\.ts$/, ".d.ts"),
+			code,
+		);
+	}
 	if (!result.success) {
 		for (const log of result.logs) {
 			console.error(log);
